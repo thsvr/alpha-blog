6a72e4c379e2915ab709af41bacb3281619af88e
thsvr <vr.ths.zd@gmail.com>
2019-11-05 19:33:51 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pages/homepage', to: 'pages#homepage'
+  get 'pages/about', to: 'pages#about'
 end
