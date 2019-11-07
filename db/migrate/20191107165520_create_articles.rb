3b5c98243fae4150008a90354f3ac1fbc9316d35
thsvr <vr.ths.zd@gmail.com>
2019-11-07 13:58:13 -0300

    deleted first files of articles and add a new ones

--- db/migrate/20191107165520_create_articles.rb
+++ db/migrate/20191107165520_create_articles.rb
@@ -2,9 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.description :text
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.text :description
 
       t.timestamps
     end
