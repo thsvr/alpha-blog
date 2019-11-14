abc91b0c0caab972bcb7aca0bfb8a61003520b5c
thsvr <vr.ths.zd@gmail.com>
2019-11-14 14:11:39 -0300

    add instance variable to an update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,13 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successful updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
 
     def show
